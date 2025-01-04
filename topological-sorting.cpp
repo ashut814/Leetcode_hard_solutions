26f7c183e36f5a539b4db51495357a2e919d6b87
Ashutosh tripathi <78640870+ashut814@users.noreply.github.com>
2025-01-04 09:42:39 +0530

    Update topological-sorting.cpp using Kahn's Algo

--- topological-sorting.cpp
+++ topological-sorting.cpp
@@ -27,3 +27,49 @@
         }
         return ans;
     }
+
+
+
+
+
+
+
+---------------------  USING KAHN'S ALGO ---------------------------------------------------
+
+    vector<int> topologicalSort(vector<vector<int>>& adj) {
+        int V = adj.size();
+        queue<int>q;
+        vector<int> inDegree(V,0);
+        
+        // step 1: get the indegree
+        for(int u = 0; u < V; u++){
+            for(auto &v : adj[u]){
+                inDegree[v]++;
+            }
+        }
+        
+        for(int i = 0 ; i < V; i++){
+            if(inDegree[i] == 0){
+                q.push(i);
+            }
+        }
+        
+        
+        vector<int>result;
+        // write the bfs logic
+        
+        while(!q.empty()){
+            int u = q.front();
+            result.push_back(u);
+            q.pop();
+            
+            for(int &v : adj[u]){
+                inDegree[v]--;
+                if(inDegree[v] == 0){
+                    q.push(v);
+                }
+            }
+        }
+        return result;
+        
+    }
