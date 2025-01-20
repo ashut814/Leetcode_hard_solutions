a13d71d96a428b129d987d3e0e6be192aa6eb8bd
Ashutosh tripathi <78640870+ashut814@users.noreply.github.com>
2025-01-20 20:06:00 +0530

    Create Minimum Window Substring.cpp

---
+++ Minimum Window Substring.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        int n = s.length();
+        map<char,int>mp;
+        for(char &ch : t){
+            mp[ch]++;
+        }
+
+        int reqSize = t.length();
+        int i = 0, j = 0;
+        int minWindow = INT_MAX;
+        int minStart = 0;
+        while(j < n){
+            char ch_j = s[j];
+            if(mp[ch_j] > 0)
+                reqSize--;
+            mp[ch_j]--;
+            while(reqSize == 0){
+                if(minWindow > j-i+1){
+                    minWindow = j-i+1;
+                    minStart = i;
+                }
+                char ch_i = s[i];
+                mp[ch_i]++;
+                if(mp[ch_i] > 0)
+                    reqSize++;
+                i++;
+            }
+            j++;
+        }
+        return minWindow == INT_MAX ? "" : s.substr(minStart,minWindow);
+    }
+};
