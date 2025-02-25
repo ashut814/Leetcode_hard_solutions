7c9e0b5a5984921da3ec00e156173368a80a7370
Ashutosh tripathi <78640870+ashut814@users.noreply.github.com>
2025-02-26 05:19:42 +0530

    Time: 7 ms (18.06%) | Memory: 21.1 MB (13.86%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,23 +1,30 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& adj, int node, vector<bool>& visited) {
+    void dfs(unordered_map<int,vector<int>> &adj, int node, vector<bool>&visited){
         visited[node] = true;
-        for(int neighbor = 0; neighbor < adj.size(); neighbor++) {
-            if(adj[node][neighbor] == 1 && !visited[neighbor]) {
-                dfs(adj, neighbor, visited);
+        for(int &neighbour : adj[node]){
+            if(!visited[neighbour]) {
+                dfs(adj,neighbour,visited);
             }
         }
     }
-
-    int findCircleNum(vector<vector<int>>& adj) {
-        int V = adj.size();
-        vector<bool> visited(V, false);
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int V = isConnected.size();
+        vector<bool>visited(V,false);
+        unordered_map<int,vector<int>>adj;
+        for(int i =0 ; i < V; i++){
+            for(int j = 0 ; j < V; j++){
+                if(isConnected[i][j] == 1){
+                    adj[i].push_back(j);
+                    adj[j].push_back(i);
+                }
+            }
+        }
         int count = 0;
-
-        for(int i = 0; i < V; i++) {
-            if(!visited[i]) {
-                dfs(adj, i, visited);
-                count++;  // Count new provinces
+        for(int i = 0 ; i < V; i++){
+            if(!visited[i]){
+                dfs(adj,i,visited);
+                count++;
             }
         }
         return count;
