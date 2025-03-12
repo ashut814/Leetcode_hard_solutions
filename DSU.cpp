f7cb9b35631b0586763a83d3282668e666f3ca5b
Ashutosh tripathi <78640870+ashut814@users.noreply.github.com>
2025-03-12 21:11:43 +0530

    Update DSU.cpp

--- DSU.cpp
+++ DSU.cpp
@@ -1,1 +1,17 @@
+int find(int element, vector<int>&parent){
+  if(element == parent[element]){
+    return element;
+  }
 
+  return find(parent[element],parent);
+}
+
+
+void union(int x, int y, vector<int>& parent){
+  int x_parent = find(x,parent);
+  int y_parent = find(y,parent);
+
+  if(x_parent != y_parent){
+    parent[x_parent] = y_parent;
+  }
+}
