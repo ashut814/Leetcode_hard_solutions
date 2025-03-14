9e697b636100e3845d249451a81f4e83ea86f5c7
Ashutosh tripathi <78640870+ashut814@users.noreply.github.com>
2025-03-14 20:34:41 +0530

    Update Detect_cycle_using_dsu.cpp

--- Detect_cycle_using_dsu.cpp
+++ Detect_cycle_using_dsu.cpp
@@ -1,42 +1,45 @@
-vector<int>rank;
-vector<int>parent;
+class Solution
+{
+    public:
+    vector<int>rank;
+    vector<int> parent;
+    int find(int x){
+        if(x == parent[x]) return x;
+        return parent[x] = find(parent[x]);
+    }
+    
+    void Union(int x, int y){
+        int x_parent = find(x);
+        int y_parent = find(y);
+        
+        if(x_parent > y_parent){
+            parent[y_parent] = x_parent;
+        } else if(x_parent < y_parent){
+            parent[x_parent] = y_parent;
+        } else{
+            parent[x_parent] = y_parent;
+            rank[y_parent]++;
+        }
+    }
+    int detectCycle(int V, vector<int> adj[]) {
+        rank.resize(V, 0);
+        parent.resize(V);
 
-int find(int x){
- if(x == parent[x]) return x;
+        for (int i = 0; i < V; i++) {
+            parent[i] = i;
+        }
 
- return parent[x] = find(parent[x]);
-}
-
-void union(int x, int y){
- int parent_x = parent[x];
- int parent_y = parent[y];
-
- if(rank[parent_x] > rank[parent_y]){
-parent[y_parent] = x_parent;
-} else if(rank[parent_y] < rank[parent_x]){
-parent[x_parent] = y_parent;
-} else{
-parent[x_parent] = y_parent;
-rank[y_parent]++;
-}
-}
-
-
-int detectCycle(int V, vector<int>adj[]){
-parent.size(V);
-rank.resize(V,0);
-
-for(int u = 0; u < V; u++){
-for(int& v : adj[u]){
-if(u < v){
-if(find(u) == find(v){
-return true;
-} else{
-union(u,v);
-}
-}
-}
-}
-return false;
-}
-
+        for (int u = 0; u < V; u++) {
+            for (int& v : adj[u]) {
+                if (u < v) {
+                    if (find(u) == find(v)) {
+                        return true;
+                    } else {
+                        Union(u, v);
+                    }
+                }
+            }
+        }
+        return false; 
+    }
+};
