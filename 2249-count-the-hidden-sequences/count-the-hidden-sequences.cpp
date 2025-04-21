a43a208077fc532239dd66fa6c35ed3ee93dcbb6
Ashutosh tripathi <78640870+ashut814@users.noreply.github.com>
2025-04-21 15:20:29 +0530

    Time: 2 ms (41.13%) | Memory: 112.7 MB (16.94%) - LeetSync

--- 2249-count-the-hidden-sequences/count-the-hidden-sequences.cpp
+++ 2249-count-the-hidden-sequences/count-the-hidden-sequences.cpp
@@ -17,3 +17,30 @@ public:
         return max(0LL, maxX - minX + 1);
     }
 };
+
+
+// differences[i] = hidden[i+1] - hidden[i]
+// differences = [2, -1, 3]
+// hidden[1] is 2 more than hidden[0]
+// hidden[2] is 1 more than hidden[1]
+// hidden[3] is 2 more than hidden[2]
+// lets say hidden[0] = x
+// hidden[1] = x + 2 => because difference[0] = 2
+// hidden[2] =  (x+2) - 1 = x + 1 =>  // because difference[1] = -1
+// hidden[3] = (x+1) + 3 = x + 4 =>  because difference[2] = 3
+
+// so now hidden array is : [x, x + 2, x + 1, x + 4]
+
+
+// prefixSum = [0,1,-2,2]
+
+// hidden[i] = x + prefix[0]
+
+// minPrefix = -2;
+// maxPrefix = 2
+
+// 1 <= x -2 => x >= 3
+// 6 >= x + 2 => x <= 4
+// x E [3,4]
+// ans = 4-3+1 => 2
+
