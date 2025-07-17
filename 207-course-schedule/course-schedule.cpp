f0bb71945f72018b3e22b1a1300c76f385488348
Ashutosh tripathi <78640870+ashut814@users.noreply.github.com>
2025-07-17 22:50:54 +0530

    Update course-schedule.cpp

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -6,7 +6,7 @@ public:
         for(int i = 0 ; i < n; i++){
             if(inDegree[i] == 0){
                 q.push(i);
-                count++;
+                count++; 
             }
         }
  
